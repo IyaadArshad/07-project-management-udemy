bf5e0acf19a2d2d87f14bf0668d49db1adabb549
IyaadArshad <hello@iyaadarshad.com>
2026-04-16 16:42:07 +0400

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from './components/Sidebar'
+function App() {
+  return (
+    <>
+    <Sidebar />
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
