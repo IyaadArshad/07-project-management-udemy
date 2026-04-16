8cb41084b9ed35796b16b876be701a35fd2fc3b1
IyaadArshad <hello@iyaadarshad.com>
2026-04-16 16:42:10 +0400

    Add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+export default function Sidebar() {
+    return (
+        <>
+            <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-72 h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
+                    <ul className="text-3xl font-bold ml-2 mb-4 mt-2">
+                        <h1>Your projects</h1>
+                    </ul>
+                    <ul className="flex justify-center items-center">
+                        <button class="bg-blue-500 rounded-[999px] hover:bg-blue-700 text-white font-bold py-2 px-4 w-[90%] my-2 rounded">
+                            + Add Project
+                        </button>
+                    </ul>
+                </div>
+            </aside>
+        </>
+    )
+}
