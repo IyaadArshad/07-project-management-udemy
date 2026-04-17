a967e2d5f508c683ff170e87972fc7e66e4e5412
IyaadArshad <hello@iyaadarshad.com>
2026-04-17 18:31:01 +0400

    style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,20 +24,20 @@ export default function Sidebar({ creatingProject, setOpenProject, setIsCreating
 
     return (
         <>
-            <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-96 h-full transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
+            <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-96 h-full mt-12 rounded-tr-[8px] transition-transform -translate-x-full sm:translate-x-0 bg-[#593808]" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft">
                     <ul className="text-3xl font-bold ml-2 mb-4 mt-2">
                         <h1>Your projects</h1>
                     </ul>
                     <ul className="flex justify-center items-center">
-                        <button onClick={CreateProject} className="bg-zinc-50 border-zinc-300 border rounded-[12px] text-lg hover:bg-zinc-100 py-3 px-4 w-[95%] my-2 rounded">
-                            + Add Project
+                        <button onClick={CreateProject} className="bg-[#593808] border text-lg border-[#9d5a0b] rounded-[12px] hover:bg-[#492f07] py-3 px-4 w-[95%] my-2">
+                            + Add New Project
                         </button>
                     </ul>
                     {projectList ? projectList.projectList.map(project => <div className='mt-4 flex justify-center items-center'>
-                        <button onClick={() => setOpenProject(project)} className='bg-zinc-50 border border-zinc-300 hover:bg-zinc-100 rounded-[12px] px-3 py-4 w-[95%]'>
+                        <button onClick={() => setOpenProject(project)} className='bg-[#593808] border text-lg border-[#9d5a0b] rounded-[12px] hover:bg-[#492f07] px-3 py-4 w-[95%]'>
                             <h1 className='text-2xl mb-1 font-semibold text-start'>{project.title}</h1>
-                            <p className='text-start text-gray-700'>Due on {project.dueDate}</p>
+                            <p className='text-start text-[#c7c4a4]'>Due on {project.dueDate}</p>
                         </button>
                     </div>) : null}
                 </div>
