92267f7330e1b4873c6f2184eb7044639be342e1
IyaadArshad <hello@iyaadarshad.com>
2026-04-17 15:02:45 +0400

    move state up

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import { useState } from 'react';
 
 function App() {
   const [creatingProject, setIsCreatingProject] = useState(false);
-  const [openProject, setOpenProject] = useState("");
+  const [openProject, setOpenProject] = useState(0);
 
   return (
     <>
       <Sidebar
         setIsCreatingProject={setIsCreatingProject}
-        creatingProject={creatingProject} />
+        creatingProject={creatingProject}
+        setOpenProject={setOpenProject} />
       <ProjectScreen
-      openProject={openProject}
+        openProject={openProject}
         setIsCreatingProject={setIsCreatingProject}
         creatingProject={creatingProject} />
     </>
